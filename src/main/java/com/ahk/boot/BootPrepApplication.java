c760c43f62374495ad5f851e81fd5c47ac32db16
Aasif <asif.dor88@gmail.com>
2025-06-04 01:59:48 +0530

    Initial Commit for learning

---
+++ src/main/java/com/ahk/boot/BootPrepApplication.java
@@ -0,0 +1,13 @@
+package com.ahk.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPrepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahk/boot/BootPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahk.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
